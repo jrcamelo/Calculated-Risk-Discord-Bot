1c49313d0f1a7d05ea6dc5777e8c20ca3e35707f
João Rafael <joaorafaelx@gmail.com>
2021-09-24 15:17:15 -0300

    Add Right command... funny

--- Bot/commands/util/WhoRight.js
+++ Bot/commands/util/WhoRight.js
@@ -1,1 +1,14 @@
-// Haha funny
+const BaseCommand = require("../base_command.js");
+const HelpPresenter = require("../../presenters/help_presenter.js");
+
+module.exports = class HelpCommand extends BaseCommand  {
+  static aliases = ["Right"]
+  static description = "Funny"
+  static argsDescription = "funny"
+
+  canDelete = true
+
+  async execute() {
+    return this.replyDeletable("Very funny, huh?")
+  }
+}
